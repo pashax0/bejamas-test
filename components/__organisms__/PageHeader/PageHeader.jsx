2523ba330f1b9425592c657217fef1acbe6bee20
pashax0 <pashax0@gmail.com>
2021-11-22 14:32:51 +0300

    x0_1: feature: update global page styles, remove redundant code

--- components/__organisms__/PageHeader/PageHeader.jsx
+++ components/__organisms__/PageHeader/PageHeader.jsx
@@ -1,11 +1,13 @@
+import classNames from 'classnames';
+
 import Logo from '../../__atoms__/Logo';
 import Basket from '../../__atoms__/Basket';
 
 import cssStyles from './x0.module.css';
 
-function PageHeader() {
+function PageHeader({ className }) {
   return (
-    <header className={cssStyles.header}>
+    <header className={classNames(cssStyles.header, className)}>
       <Logo />
       <Basket products={[1]} />
     </header>
