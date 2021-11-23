c99099b04864bc9446bb5a5a1b326034b558b0a4
pashax0 <pashax0@gmail.com>
2021-11-23 14:58:17 +0300

    x0_1: feature: separate Filter component

--- components/__organisms__/Storefront/Storefront.jsx
+++ components/__organisms__/Storefront/Storefront.jsx
@@ -1,21 +1,19 @@
 import { useEffect, useState } from 'react';
-import { useRouter } from 'next/router';
-import dynamic from 'next/dynamic';
+// import dynamic from 'next/dynamic';
 import classNames from 'classnames';
 
 import useWindowSize from '../../../hooks/useWindowSize';
-import { dummyProducts, categories, priceBreakpoints } from '../../../data/seed';
-import { adoptPrices } from '../../../api/adapters/prices';
+import { dummyProducts } from '../../../data/seed';
 import Products from '../../__molecules__/Products';
-import Checkbox from '../../__atoms__/Checkbox';
 import Pagination from '../../__atoms__/Pagination';
+import Filter from '../../__molecules__/Filter';
 import { BotttomArrowIcon } from './BotttomArrowIcon';
 import { TopArrowIcon } from './TopArrowIcon';
 import { FilterIcon } from '../../__icons__/FilterIcon';
 
 import cssStyles from './x0.module.css';
 
-const Modal = dynamic(() => import('../../__molecules__/Modal'), { ssr: false });
+// const Modal = dynamic(() => import('../../__molecules__/Modal'), { ssr: false });
 
 const fetcher = (
   page,
@@ -65,97 +63,64 @@ const fetcher = (
 function Storefront({
   className,
 }) {
-  const router = useRouter();
-
-  const initialFilter = {
-    categories: [],
-    priceConditions: [],
+  // const [isModalOpened, openModal] = useState(false);
+  const defaultProductParameters = {
+    filter: undefined,
+    sort: { by: 'name', direction: 'asc' },
+    currentPage: 1,
   };
-
-  const prices = adoptPrices(priceBreakpoints);
-
-  const [isModalOpened, openModal] = useState(false);
-  const [currentPage, updateCurrentPage] = useState(1);
-  const [filter, updateFilter] = useState(initialFilter);
+  const [productParameters, updateProductParameters] = useState(defaultProductParameters);
   const [products, updateProducts] = useState(null);
   const [productsCount, updateProductsCount] = useState(null);
-  const [sort, updateSorting] = useState({ by: 'name', direction: 'asc' });
 
   const MOBILE_FILTER_BREAKPOINT = 768;
   const { width: screenWidth } = useWindowSize();
 
   const showDesktopFilter = screenWidth > MOBILE_FILTER_BREAKPOINT;
 
-  const { by: sortBy, direction: sortingDirection } = sort;
+  const { by: sortBy, direction: sortingDirection } = productParameters.sort;
 
   useEffect(() => {
     async function fetchProducts() {
-      const data = await fetcher(currentPage, sort, filter);
+      const data = await fetcher(productParameters.currentPage, productParameters.sort, productParameters.filter);
       updateProducts(data.products);
       updateProductsCount(data.count);
     }
 
     fetchProducts();
-  }, [sort, filter, currentPage]);
+  }, [productParameters]);
+
+  const filterChangeHandler = (newFilter) => {
+    updateProductParameters((prevParameters) => ({
+      ...prevParameters,
+      filter: newFilter,
+      currentPage: 1,
+    }));
+  };
 
   const sortingDirectionHandler = () => {
-    updateSorting((prevSorting) => ({
-      ...prevSorting,
-      direction: prevSorting.direction === 'asc' ? 'desc' : 'asc',
+    updateProductParameters((prevParameters) => ({
+      ...prevParameters,
+      sort: { ...prevParameters.sort, direction: prevParameters.sort.direction === 'asc' ? 'desc' : 'asc' },
+      currentPage: 1,
     }));
-
-    // TODO: handle router correctly for different parameters
-    router.push('?sort=asc', undefined, { shallow: true });
   };
 
   const sortingTypeHandler = (event) => {
     const type = event.target.value;
 
-    updateSorting((prevSorting) => ({
-      ...prevSorting,
-      by: type,
+    updateProductParameters((prevParameters) => ({
+      ...prevParameters,
+      sort: { ...prevParameters.sort, by: type },
+      currentPage: 1,
     }));
-    updateCurrentPage(1);
-
-    router.push(`?sortBy=${type}`, undefined, { shallow: true });
   };
 
-  const filterCategoryHandler = (event) => {
-    updateFilter((prevFilter) => {
-      const updatedCategory = event.target.name;
-      const isUpdatedCategoryChecked = event.target.checked;
-      const { categories: prevCategories } = prevFilter;
-
-      let updatedCategories = [];
-
-      if (isUpdatedCategoryChecked) {
-        updatedCategories = [...prevCategories, updatedCategory];
-      } else {
-        updatedCategories = prevCategories.filter((category) => category !== updatedCategory);
-      }
-
-      return { ...prevFilter, categories: updatedCategories };
-    });
-    updateCurrentPage(1);
-  };
-
-  const filterPriceHandler = (event) => {
-    updateFilter((prevFilter) => {
-      const updatedPrice = event.target.name;
-      const isUpdatedPriceChecked = event.target.checked;
-      const { priceConditions: prevPriceConditions } = prevFilter;
-
-      let updatedPrices = [];
-
-      if (isUpdatedPriceChecked) {
-        updatedPrices = [...prevPriceConditions, updatedPrice];
-      } else {
-        updatedPrices = prevPriceConditions.filter((condition) => condition !== updatedPrice);
-      }
-
-      return { ...prevFilter, priceConditions: updatedPrices };
-    });
-    updateCurrentPage(1);
+  const updateCurrentPage = (page) => {
+    updateProductParameters((prevParameters) => ({
+      ...prevParameters,
+      currentPage: page,
+    }));
   };
 
   const renderProducts = (renderedProducts) => (
@@ -166,50 +131,9 @@ function Storefront({
     )
   );
 
-  const renderFilters = () => (
-    <div className={classNames(cssStyles.storefront__filters, cssStyles.filters)}>
-      <div className={cssStyles.filter}>
-        <h3 className={cssStyles.filterHeader}>Category</h3>
-        <ul className={cssStyles.productFilter}>
-          {categories.map((category, i) => (
-            // eslint-disable-next-line react/no-array-index-key
-            <li key={i} className={cssStyles.productFilter__item}>
-              {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-              <label className={cssStyles.filterLabel}>
-                <Checkbox className={cssStyles.filterCheckbox} name={category} onChange={filterCategoryHandler} />
-                {category}
-              </label>
-            </li>
-          ))}
-        </ul>
-      </div>
-      <div className={cssStyles.filter}>
-        <h3 className={cssStyles.filterHeader}>
-          Price range
-        </h3>
-        <ul className={cssStyles.productFilter}>
-          {prices.map((price, i) => (
-            // eslint-disable-next-line react/no-array-index-key
-            <li key={i} className={cssStyles.productFilter__item}>
-              {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-              <label className={cssStyles.filterLabel}>
-                <Checkbox
-                  className={cssStyles.filterCheckbox}
-                  name={price.condition}
-                  onChange={filterPriceHandler}
-                />
-                {price.description}
-              </label>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </div>
-  );
-
-  const openFiltersClickHandler = () => {
-    openModal(true);
-  };
+  // const openFiltersClickHandler = () => {
+  //   openModal(true);
+  // };
 
   return (
     <div className={classNames(cssStyles.storefront, className)}>
@@ -239,24 +163,25 @@ function Storefront({
             type="button"
             aria-label="open filters"
             className={cssStyles.filterButton}
-            onClick={openFiltersClickHandler}
+            // onClick={openFiltersClickHandler}
           >
             <FilterIcon />
           </button>
         )}
       </header>
       <div className={classNames(cssStyles.storefront__main)}>
-        {showDesktopFilter ? (
-          renderFilters()
-        ) : (
-          isModalOpened && (
-          <Modal onModalClose={() => { openModal(false); }}>
-            <div>
-              {renderFilters()}
-            </div>
-          </Modal>
-          )
-        )}
+        <Filter onFilterChange={filterChangeHandler} />
+        {/* {true ? ( */}
+        {/*  renderFilters() */}
+        {/* ) : ( */}
+        {/*  isModalOpened && ( */}
+        {/*  <Modal onModalClose={() => { openModal(false); }}> */}
+        {/*    <div> */}
+        {/*      {renderFilters()} */}
+        {/*    </div> */}
+        {/*  </Modal> */}
+        {/*  ) */}
+        {/* )} */}
         <div className={classNames(cssStyles.storefront__products, cssStyles.products)}>
           {products ? (
             renderProducts(products)
@@ -264,7 +189,11 @@ function Storefront({
             <div>loading...</div>
           )}
           {(products?.length > 0 && productsCount) && (
-            <Pagination countOfProducts={productsCount} currentPage={currentPage} onPageClick={updateCurrentPage} />
+            <Pagination
+              countOfProducts={productsCount}
+              currentPage={productParameters.currentPage}
+              onPageClick={updateCurrentPage}
+            />
           )}
         </div>
       </div>
